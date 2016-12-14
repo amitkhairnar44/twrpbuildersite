2d20b329f902df8726a12bbea46c7d436fc6da55
Amit Khairnar <amitkhairnar44@gmail.com>
2016-12-14 22:24:36 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,14 @@
 		});
 
 	$(function() {
-
+		var form = document.querySelector(“form”);
+form.addEventListener(“submit”, function(event) {
+event.preventDefault();
+var timestamp = Number(new Date());
+var storageRef = firebase.storage().ref(timestamp.toString());
+	var $ = jQuery;
+var file_data = $(‘#uploadZip’).prop(‘files’)[0];
+		  storageRef.put(file_data);
 		var	$window = $(window),
 			$body = $('body');
 
